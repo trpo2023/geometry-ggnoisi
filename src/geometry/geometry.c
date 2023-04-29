b6f799c387f7b0f339045dd12d9ce613b86d8a5a
Mihail <ggnoese@mail.ru>
2023-04-29 12:04:52 +0700

    Modifi geometry.c function and Recreated .txt

--- src/geometry/geometry.c
+++ src/geometry/geometry.c
@@ -7,47 +7,17 @@
 
 #include <libgeometry/check.h>
 
-void token(char* a, float* x, float* y, float* radius) // change
+void token(char* a, float* x, float* y, float* radius)
 {
     float square, perimeter;
     char del[] = "circle( ,)";
-    *x = atof(strtok(a, del));                                      // change
-    *y = atof(strtok(NULL, del));                                   // change
-    *radius = atof(strtok(NULL, del));                              // change
-    square = M_PI * *radius * *radius;                              // change
-    perimeter = 2 * M_PI * *radius;                                 // change
-    printf("x = %.3f\ty = %.3f\tradius = %.3f\n", *x, *y, *radius); // change
+    *x = atof(strtok(a, del));
+    *y = atof(strtok(NULL, del));
+    *radius = atof(strtok(NULL, del));
+    square = M_PI * *radius * *radius;
+    perimeter = 2 * M_PI * *radius;
+    printf("x = %.3f\ty = %.3f\tradius = %.3f\n", *x, *y, *radius);
     printf("square = %.3f\tperimeter = %.3f\n", square, perimeter);
-}
-
-// change
-int intersects(float* x_arr, float* y_arr, float* radius_arr, int figure_amount)
-{
-    printf("\nIntersections:\n");
-    for (int i = 0; i < figure_amount; i++) {
-        printf("\ncircle %d. intersects circle(s) ", i);
-        for (int j = 0; j < figure_amount && j != i; j++) {
-            double dist_btw_centers = sqrt(
-                    pow(x_arr[j] - x_arr[i], 2) + pow(y_arr[j] - y_arr[i], 2));
-            if (dist_btw_centers == 0 && radius_arr[i] == radius_arr[j]) {
-                printf("%d. ", j);
-                // intersects
-                return 0;
-            }
-
-            if (radius_arr[i] + radius_arr[j] >= dist_btw_centers
-                && radius_arr[i] + dist_btw_centers >= radius_arr[j]
-                && radius_arr[j] + dist_btw_centers >= radius_arr[i]) {
-                printf("%d. ", j);
-                // intersects
-                return 0;
-            } else {
-                // does not intersect
-                return 1;
-            }
-        }
-    }
-    return 1;
 }
 
 int main()
@@ -77,10 +47,10 @@ int main()
     length = count;
     fclose(file1);
 
-    int figure_amount = 0;                                             // change
-    float* x_arr = (float*)malloc(figure_amount * sizeof(float));      // change
-    float* y_arr = (float*)malloc(figure_amount * sizeof(float));      // change
-    float* radius_arr = (float*)malloc(figure_amount * sizeof(float)); // change
+    int figure_amount = 0;
+    float* x_arr = (float*)malloc(figure_amount * sizeof(float));
+    float* y_arr = (float*)malloc(figure_amount * sizeof(float));
+    float* radius_arr = (float*)malloc(figure_amount * sizeof(float));
 
     char a[length], b[6] = "circle";
     file = fopen("geometry.txt", "r");
@@ -115,24 +85,19 @@ int main()
 
         if (error == 0) {
             printf("No Errors!\n");
-            float x = 0, y = 0, radius = 0;     // change
-            token(a, &x, &y, &radius);          // change
-            x_arr[figure_amount] = x;           // change
-            y_arr[figure_amount] = y;           // change
-            radius_arr[figure_amount] = radius; // change
-            figure_amount += 1;                 // change
+            float x = 0, y = 0, radius = 0;
+            token(a, &x, &y, &radius);
+            x_arr[figure_amount] = x;
+            y_arr[figure_amount] = y;
+            radius_arr[figure_amount] = radius;
+            figure_amount += 1;
         }
 
         error = 0;
         printf("\n");
     }
 
-    intersects(x_arr, y_arr, radius_arr, figure_amount); // change
-    puts("\n");                                          // change
-
-    free(x_arr);      // change
-    free(y_arr);      // change
-    free(radius_arr); // change
+    intersects(x_arr, y_arr, radius_arr, figure_amount);
 
     return 0;
 }
