7749db9b037c4de6c631210468abe40028707a84
Mihail <ggnoese@mail.ru>
2023-04-29 11:41:30 +0700

    Add a test group CHECK_INPUT

--- test/main.c
+++ test/main.c
@@ -4,7 +4,7 @@
 
 #define CTEST_SEGFAULT
 
-#include "C:/Users/latsu/GitHub_projects/geometry/geometry-danyowoj/thirdparty/ctest.h"
+#include "thirdparty/ctest.h"
 
 int main(int argc, const char* argv[])
 {
